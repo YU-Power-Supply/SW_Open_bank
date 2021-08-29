99d97aeb362854bde21c2aa6b95552a461116e19
kyopark <45002168+123qpq@users.noreply.github.com>
2021-08-29 19:57:45 +0900

    Update darknet_setting.py
    update dir_list with sort and add remove_txt, encoding=UTF8

--- darknet_setting.py
+++ darknet_setting.py
@@ -1,3 +1,4 @@
+#--V 1.1--
 import sys
 import os
 import json
@@ -10,7 +11,7 @@ def create_dir(dname):
         print ('Error: Creating directory. ' +  directory)
 
 def make_data(dname, classes): #make obj.data file
-    with open(f'{dname}/obj.data', "w") as f:
+    with open(f'{dname}/obj.data', "w", encoding='UTF8') as f:
         strdata = ''
         data = {'classes = ' : '{}\n'.format(len(classes)), #count classes
         'train = ' : f'{dname}train.txt\n', # train data path file
@@ -26,7 +27,7 @@ def make_names(dname, classes): # make obj.names file
 
     print("change [region]        classes = ", len(classes))
     print("change [convolutional] filters = ", (len(classes)+5)*5)
-    with open(f'{dname}/obj.names', "w") as f:
+    with open(f'{dname}/obj.names', "w", encoding='UTF8') as f:
         f.write('\n'.join(classes))
 
 def make_train_list(dname, img_dir_list): # make train.txt file
@@ -34,7 +35,7 @@ def make_train_list(dname, img_dir_list): # make train.txt file
     for path in img_dir_list:
         file_list += [path+file for file in os.listdir(path) if file[-1] == 'g'] # collect png, jpg etc..
     print(file_list)
-    with open(f'{dname}/train.txt', "w") as f:
+    with open(f'{dname}/train.txt', "w", encoding='UTF8') as f:
         f.write('\n'.join(file_list))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -42,9 +43,9 @@ def make_train_list(dname, img_dir_list): # make train.txt file
 def rename_dir(img_dir):
     dir_list = []
     for dir_name in os.listdir(img_dir):
-        if 6 < len(dir_name): # if longer then 6, rename
-            os.rename(img_dir+dir_name, img_dir+dir_name[:5])
-        dir_list.append(img_dir+dir_name[:5] + "/")
+        if 7 < len(dir_name): # if longer then 6, rename
+            os.rename(img_dir+dir_name, img_dir+dir_name[:6])
+        dir_list.append(img_dir+dir_name[:6] + "/")
     return dir_list
 
 ## 한 번에 합쳐보리기 ##
@@ -56,16 +57,10 @@ def rename_file(dir_list):
 
 def json_to_txt(json_path, save_path, classes):
     file_list = os.listdir(json_path)
-    file_list_py = [file for file in file_list]
-    print(file_list_py)
     
-    #imglist = ''
-    for i in file_list_py:
-    #if i[-1] == 'g':
-    #    imglist += "test/img/" + i + '\n'
-
+    for i in file_list:
         data = ''
-        with open(json_path+i) as file:
+        with open(json_path+i, encoding='UTF8') as file:
             json_data = json.load(file)
             bounding_data = json_data['ObjectInfo']['BoundingBox']
             for obj in bounding_data:
@@ -74,26 +69,31 @@ def json_to_txt(json_path, save_path, classes):
                     #conversion x, y, w, h
                     transpos = '{} {} {} {}'.format(((ltx + rbx)/2)/720, ((lty + rby)/2)/1280, (rbx - ltx)/720, (rby - lty)/1280)
                     data += f"{classes[obj]} {transpos}\n"
-                
-        #print(i[:-4], data)
 
         #데이터 저장부
         savepath = save_path + i[:-4] + "txt" # json -> txt
-        with open(savepath, "w") as wfile:
+        with open(savepath, "w", encoding='UTF8') as wfile:
             wfile.write(data)
     
     
+def remove_txt(txtpath):  # when you want to remove txt files
+    for t in os.listdir(txtpath):
+        if t[-3:] == "txt":
+            os.remove(txtpath+t)
 
 if __name__ == '__main__':
     
     directory = 'custom/' # what you want to create directory name
-    img_dir = 'realimg/' #route of img_dir
-    json_dir = 'real/'    #route of json_dir
+    img_dir = 'Training/customboximg/' #route of img_dir
+    json_dir = 'Training/custombox/'    #route of json_dir
     classes = {'Face' : 0, 'Leye' : 1, 'Reye' : 2, 'Mouth' : 3, 'Phone' : 4, 'Cigar' : 5}
     
     img_dir_list = rename_dir(img_dir)
     json_dir_list = rename_dir(json_dir)
-    print(img_dir_list)
+
+    img_dir_list.sort()
+    json_dir_list.sort()
+
     if len(sys.argv) == 1:
         print("명령 프롬프트로 실행하세요")
         exit(0)
@@ -104,5 +104,10 @@ if __name__ == '__main__':
         make_train_list(directory, img_dir_list)
     elif sys.argv[1] == "jtt":
         for i in range(len(json_dir_list)):
+            print(i)
             json_to_txt(json_dir_list[i], img_dir_list[i], classes)
         print("완료")
+    elif sys.argv[1] == "rmv":
+        for i in img_dir_list:
+            print(i)
+            remove_txt(i)
