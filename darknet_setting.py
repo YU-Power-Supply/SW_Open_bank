201acdf79ede89fe3c813699c85b92e5167a168a
kyopark <45002168+123qpq@users.noreply.github.com>
2021-09-12 19:34:27 +0900

    Update darknet_setting.py
    now can korean to english

--- darknet_setting.py
+++ darknet_setting.py
@@ -1,21 +1,21 @@
-#--V 1.1--
+#--V 1.2--
 import sys
 import os
 import json
 
 def create_dir(dname):
     try:
-        if not os.path.exists("./"+dname): # if you don't have same dir, create
-            os.makedirs("./"+dname)
+        if not os.path.exists("./"+directory): # if you don't have same dir, create
+            os.makedirs("./"+directory)
     except OSError:
-        print ('Error: Creating directory. ' +  dname)
+        print ('Error: Creating directory. ' +  directory)
 
 def make_data(dname, classes): #make obj.data file
     with open(f'{dname}/obj.data', "w", encoding='UTF8') as f:
         strdata = ''
         data = {'classes = ' : '{}\n'.format(len(classes)), #count classes
         'train = ' : f'{dname}train.txt\n', # train data path file
-        'valid = ' : f'{dname}train.txt\n', # valid data path file
+        'valid = ' : f'{dname}valid.txt\n', # valid data path file
         'names = ' : f'{dname}obj.names\n', # classes name file
         'backup = ' : 'backup/\n'}           # storage path
 
@@ -30,12 +30,14 @@ def make_names(dname, classes): # make obj.names file
     with open(f'{dname}/obj.names', "w", encoding='UTF8') as f:
         f.write('\n'.join(classes))
 
-def make_train_list(dname, img_dir_list): # make train.txt file
+def make_train_list(dname, img_dir_list, check): # make train.txt file
     file_list = []
     for path in img_dir_list:
         file_list += [path+file for file in os.listdir(path) if file[-1] == 'g'] # collect png, jpg etc..
-    print(file_list)
-    with open(f'{dname}/train.txt', "w", encoding='UTF8') as f:
+    #print(file_list)
+    with open(f'{dname}train.txt', check, encoding='UTF8') as f:
+        if check == "a":
+            f.write("\n")
         f.write('\n'.join(file_list))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -52,6 +54,15 @@ def rename_dir(img_dir):
 def rename_file(dir_list):
     for file_name in dir_list:
         pass # 보류
+
+def kor_to_eng(dname, img_dir_list):
+    check = {"정상주시":"normal", "졸음재현":"drowse", "하품재현":"yawn", "흡연재현":"smoke", "통화재현":"call"}
+    for path in img_dir_list:
+        for file in os.listdir(path): # collect png, jpg etc..
+            temp = file.split("_")
+            if temp[4].isdigit():
+                continue
+            os.rename(path+file, path + "_".join(temp[:3] + [check[temp[5]]] + temp[-3:]))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -84,8 +95,8 @@ def remove_txt(txtpath):  # when you want to remove txt files
 if __name__ == '__main__':
     
     directory = 'custom/' # what you want to create directory name
-    img_dir = 'Training/customboximg/' #route of img_dir
-    json_dir = 'Training/custombox/'    #route of json_dir
+    img_dir = 'D:/dataset/Training/customboximg/' #route of img_dir
+    json_dir = 'D:/dataset/Training/custombox/'    #route of json_dir
     classes = {'Face' : 0, 'Leye' : 1, 'Reye' : 2, 'Mouth' : 3, 'Phone' : 4, 'Cigar' : 5}
     
     img_dir_list = rename_dir(img_dir)
@@ -97,17 +108,30 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         print("명령 프롬프트로 실행하세요")
         exit(0)
-    elif sys.argv[1] == "set":
+
+    elif sys.argv[1] == "setnew":
         create_dir(directory)
         make_data(directory, classes)
         make_names(directory, classes)
-        make_train_list(directory, img_dir_list)
-    elif sys.argv[1] == "jtt":
+        make_train_list(directory, img_dir_list, "w")
+
+    elif sys.argv[1] == "setadd":
+        create_dir(directory)
+        make_data(directory, classes)
+        make_names(directory, classes)
+        make_train_list(directory, img_dir_list, "a")
+
+    elif sys.argv[1] == "jtt": # json to text
         for i in range(len(json_dir_list)):
             print(i)
             json_to_txt(json_dir_list[i], img_dir_list[i], classes)
-        print("완료")
-    elif sys.argv[1] == "rmv":
+
+    elif sys.argv[1] == "rmv": #remove
         for i in img_dir_list:
             print(i)
             remove_txt(i)
+    
+    elif sys.argv[1] == "kte": # korea to english
+        kor_to_eng(directory, img_dir_list)
+
+    print("완료")
