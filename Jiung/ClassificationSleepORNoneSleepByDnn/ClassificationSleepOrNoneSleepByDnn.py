98abd2c598e8fb554ef5cfe617abdcc80040a297
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-09-24 17:02:10 +0900

    Update ClassificationSleepOrNoneSleepByDnn.py

--- Jiung/ClassificationSleepORNoneSleepByDnn/ClassificationSleepOrNoneSleepByDnn.py
+++ Jiung/ClassificationSleepORNoneSleepByDnn/ClassificationSleepOrNoneSleepByDnn.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 import numpy as np
+import cv2, dlib
+from imutils import face_utils
 
-import matplotlib.pyplot as plt
 import random
 import sys
 import os
@@ -9,8 +10,8 @@ import os
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D
 from tensorflow.keras import Sequential
 from tensorflow.keras.optimizers import Adam
-
 from tensorflow.keras.models import load_model
+
 
 
 def train(savePath):
@@ -18,7 +19,7 @@ def train(savePath):
     keyPointNum = 68
 
     motionNum = 100
-    PointPerFrame = tf.constant([[[random.randrange(1, 10) for _ in range(frameNum)] for _ in range(keyPointNum)] for _ in range(motionNum)], dtype = tf.float32) # prame per points
+    PointPerFrame = tf.constant([[[random.randrange(1, 10) for _ in range(keyPointNum)] for _ in range(frameNum)] for _ in range(motionNum)], dtype = tf.float32) # prame per points
     groundTruth = tf.constant([ random.randrange(0, 2) for _ in range(motionNum)]) # sleep = 1, didn`t sleep = 0
 
 
@@ -26,7 +27,7 @@ def train(savePath):
 
     model = Sequential()
 
-    model.add( Flatten( input_shape= (68,60)))
+    model.add( Flatten( input_shape= (60,68)))
     model.add( Dense( units= 64,  activation='relu') )
     model.add( Dense( units= 10,  activation='softmax') )
     model.compile( loss='sparse_categorical_crossentropy', optimizer="adam",
@@ -36,23 +37,68 @@ def train(savePath):
     h = model.fit( PointPerFrame, groundTruth, epochs = 100)
     model.save(savePath)
     model.summary()
-
-def test(model, testPointPerFramePath):
     
-    with open(f"{testPointPerFramePath}", "r") as f:
-        testPointPerFramePath = tf.constant(f.readline(), dtype = tf.float32)
-        f.close()
-        
+
+    
+
+def test(model):
+
     model = load_model(model)
+    print("\n")
     model.summary()
+    print("\n")
 
-    model.predict(testPointPerFrame)
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
+    keyPointModel = ('models/2018_12_17_22_58_35.h5')
+
+    cap = cv2.VideoCapture(0)
+
+    testPointPerFrames = []
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        img = frame.copy()
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = detector(gray_img, 1)
+        
+        pointFrame = []
+        for face in faces:
+            shapes = predictor(gray_img, face)
+            shapes = face_utils.shape_to_np(shapes)
+            
+
+            for point in shapes:
+                cv2.circle(img, point, 5, (255, 255, 0))
+
+            for keyPoints in shapes:
+                for keyPoint in keyPoints:
+                    pointFrame.append(keyPoint)
+
+        if len(pointFrame) == 136: # landmark * 2 = 136
+            testPointPerFrames.append(pointFrame)
+            
+        if len(testPointPerFrames) == 10:
+            testPointPerFrames = tf.constant(testPointPerFrames, dtype = tf.float32)
+            model.predict(testPointPerFrames).argmax(axis = 1)
+            testPointPerFrames = []
+
+        
+
+
+        cv2.imshow('result', img)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    
+    
 
 if __name__=="__main__":
 
     model = ""
-    testPointPerFrame = ""
     savePath = ""
 
     if len(sys.argv) == 1:
@@ -62,11 +108,10 @@ if __name__=="__main__":
     elif sys.argv[1] == "train": # train save_path
         savePath = sys.argv[2]
         train(savePath)
-    elif sys.argv[1] == "test": # test model_path testFrame_path
+    elif sys.argv[1] == "test": # test model_path 
         model = f"{sys.argv[2]}"
-        testPointPerFramePath = f"{sys.argv[3]}"
 
-        test(model,testPointPerFramePath)
+        test(model)
 
     else :
         print("잘못 된 명령어 입니다.")
