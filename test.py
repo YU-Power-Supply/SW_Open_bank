d0847bda2ee1534776f5df1376c8211b197ebe76
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-09-24 18:01:48 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-
-pred_l = 0.8
-pred_r = 0.1
-
-state_l = f'O [{pred_l}%]' if pred_l > 0.1 else f'- [{pred_l}%]'
-state_r = f'O [{pred_r}%]' if pred_r > 0.1 else f'- [{pred_r}%]'
-
-print(f"steate_l : {state_l}, state_r : {state_r}")
