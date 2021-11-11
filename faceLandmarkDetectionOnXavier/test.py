635806351a4fc55341ec610bca54c8967d9059a4
123qpq <45002168+123qpq@users.noreply.github.com>
2021-11-11 09:52:55 +0900

    Update test.py

--- faceLandmarkDetectionOnXavier/test.py
+++ faceLandmarkDetectionOnXavier/test.py
@@ -2,7 +2,7 @@ import facetracker_custom as fc
 import numpy as np
 
 
-#yeild함수, fps15로 2초마다 68*30프레임 numpy배열 뽑아줌
+#yield함수, fps15로 2초마다 68*30프레임 numpy배열 뽑아줌
 
 for f in fc.run(visualize=1, max_threads=4, capture="video.mp4"):
     
