37fb6a2daac153b61ca0242aa915cb3237b1bb6e
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-11-15 20:12:23 +0900

    Update JiungsImportEx.py

--- faceLandmarkDetectionOnXavier/JiungsImportEx.py
+++ faceLandmarkDetectionOnXavier/JiungsImportEx.py
@@ -1,21 +1,26 @@
 import facetracker_custom as fc
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 
-#yield함수, fps15로 2초마다 68*30프레임 numpy배열 뽑아줌
+#yield함수, fps15로 2초마다 68*30프레임 numpy배열 q뽑아줌
 
 
 
 def classificationSleepOrNonsleep(Y_Data_30_Frames):
-    Y_Data_30_Frames = tf.constant(Y_Data_30_Frames, dtype = tf.float32)
-    print(Y_Data_30_Frames)
+    ## tensor generate
+    Y_Data_30_Frames = tf.expand_dims(Y_Data_30_Frames, axis = 0) 
+    Y_Data_30_Frames = tf.expand_dims(Y_Data_30_Frames, axis = 3)
+
+    print(model.predict(Y_Data_30_Frames))
+    
 
 
 
 if __name__=="__main__":
     model = "output.h5"
-    #model = load_model(model)
-    #model.summary() # model Info
+    model = load_model(model)
+    model.summary() # model Info
 
     for Y_Data_30_Frames in fc.run(visualize=1, max_threads=4, capture=0):
         classificationSleepOrNonsleep(Y_Data_30_Frames)
