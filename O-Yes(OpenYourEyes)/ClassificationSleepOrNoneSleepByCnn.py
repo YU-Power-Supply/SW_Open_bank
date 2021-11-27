ff90cf3d1f6cb38ebb950e9e7aba16f3a76209a3
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-11-27 16:03:36 +0900

    Update ClassificationSleepOrNoneSleepByCnn.py

--- O-Yes(OpenYourEyes)/ClassificationSleepOrNoneSleepByCnn.py
+++ O-Yes(OpenYourEyes)/ClassificationSleepOrNoneSleepByCnn.py
@@ -18,8 +18,6 @@ def train(trainDataPath, saveModelPath):
     pointPerFramePerMotion = []
     frameNum = 30
     keyPointNum = 68
-
-
     
     # read trainData
     groundTruth = []
@@ -31,8 +29,8 @@ def train(trainDataPath, saveModelPath):
 
             for frames in file:
                 pointX = frames.split()
-                if len(pointX) != 68: print(f"{file}") # CHECK 68 POINT
-                pointX = list(map(int, pointX)) # convert str to integer
+                if len(pointX) != keyPointNum: print(f"{file}") # CHECK 68 POINT
+                pointX = list(map(float, pointX)) # convert str to integer
                 pointPerFrame.append(pointX)
             pointPerFramePerMotion.append(pointPerFrame)
 
@@ -44,7 +42,9 @@ def train(trainDataPath, saveModelPath):
 
     groundTruth = tf.constant(groundTruth, dtype = tf.float32) # sleep = 1, didn`t sleep = 0
     print("groundTruth`s dims : ", tf.shape(groundTruth))
-
+    print("pointPerFramePerMotion`s dims : ", tf.shape(pointPerFramePerMotion))
+    
+    
     # ''' random data generator
     # testX = tf.constant([[[random.randrange(1, 10) for _ in range(keyPointNum)] for _ in range(frameNum)] for _ in range(2)], dtype = tf.float32) # prame per points
     # testY = tf.constant([ [random.randrange(0, 2) ]for _ in range(2)]) # sleep = 1, didn`t sleep = 0
@@ -57,7 +57,7 @@ def train(trainDataPath, saveModelPath):
     # Training Model Define
     # Training Model Define ... VGGNet style 14 Layerts network model
     model = Sequential([
-        Conv2D(input_shape = (30, 68, 1), kernel_size = (3, 3), filters = 32, padding = 'same', activation = 'relu'),
+        Conv2D(input_shape = (frameNum, keyPointNum, 1), kernel_size = (3, 3), filters = 32, padding = 'same', activation = 'relu'),
         Conv2D(kernel_size = (3, 3), filters = 64, padding = 'same', activation = 'relu'),
         MaxPool2D(pool_size = (2, 2)),
         Dropout(rate = 0.5),
@@ -68,13 +68,14 @@ def train(trainDataPath, saveModelPath):
         Dropout(rate = 0.5),
 
         Flatten(),
-        Dense(units = 512, activation = 'relu'),
-        Dropout(rate = 0.5),
         Dense(units = 256, activation = 'relu'),
         Dropout(rate = 0.5),
-        Dense(units = 10, activation = 'sigmoid')
+        Dense(units = 64, activation = 'relu'),
+        Dropout(rate = 0.5),
+        Dense(units = 16, activation = 'relu'),
+        Dropout(rate = 0.5),
+        Dense(units = 2, activation = 'softmax')
         
-
     ])
 
     # model.add( Dense( units= 1,  activation='sigmoid') )
@@ -82,74 +83,11 @@ def train(trainDataPath, saveModelPath):
                   metrics=['acc'] )  # ! gradinet descent 종류 더 알아보기, sparse_categorical_crossentropy 등등 더 있음
 
     ## Moddel Training
-    h = model.fit( pointPerFramePerMotion, groundTruth, epochs = 10)
-    model.save(savePath)
+    model.fit( pointPerFramePerMotion, groundTruth, epochs = 2000)
+    model.save(saveModelPath)
+    
     model.summary()
 
-def test(model):
-
-    model = load_model(model)
-    print("")
-    model.summary()
-    print("")
-
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
-
-    keyPointModel = ('models/2018_12_17_22_58_35.h5')
-
-    cap = cv2.VideoCapture(0)
-
-    testPointPerFrames = []
-    startTime = time.time()
-    while cap.isOpened():
-        ret, frame = cap.read()
-
-        if not ret:
-            break
-
-        img = frame.copy()
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = detector(gray_img, 1)
-        
-        pointFrame = []
-        for face in faces:
-            shapes = predictor(gray_img, face)
-            shapes = face_utils.shape_to_np(shapes)
-            
-
-            for point in shapes:
-                cv2.circle(img, point, 5, (255, 255, 0))
-
-            for keyPoints in shapes:
-                for keyPoint in keyPoints:
-                    pointFrame.append(keyPoint)
-
-        if len(pointFrame) == 136: # landmark * 2 = 136
-            testPointPerFrames.append(pointFrame)
-            print("Counted Frame Number", len(testPointPerFrames))
-        
-        if len(testPointPerFrames) == 25:
-            # print(testPointPerFrames)
-            testPointPerFrames = tf.constant(testPointPerFrames, dtype = tf.float32)
-            
-            # print(testPointPerFrames.shape) # Dims
-            testPointPerFrames = tf.expand_dims(testPointPerFrames, axis=0)
-            
-            # print(testPointPerFrames.shape) # reshaped Dims
-
-            print(model.predict(testPointPerFrames).argmax(axis =1))
-            endTime = time.time()
-            print("Time: " ,endTime - startTime)
-            
-            testPointPerFrames = []
-
-        
-
-
-        cv2.imshow('result', img)
-        if cv2.waitKey(1) == ord('q'):
-            break
 
 def mkdirs(dirPath, fromNum, toNum):
     for i in range(int(fromNum), int(toNum) +1):
@@ -168,15 +106,9 @@ def dataPreprocessing(imgPath, fromMotion, toMotion):
 
     
     groundTruth = "1"
-    
     motionCnt = 0
-    frameNum = 30
-    keyPointNum = 68
-
-    
  
     for motion in range(int(fromMotion),int(toMotion)+1 ):
-        pointPerFrame = []
         motionCnt += 1
         frameCnt = 0
         
@@ -190,6 +122,7 @@ def dataPreprocessing(imgPath, fromMotion, toMotion):
 
         for frame in frameList: 
             img = cv2.imread(f"{imgPath}/{motion}/{frame}")
+            height, width, color =  img.shape # get img shape for nomalization
             faces = detector(img, 1)
             
             if(len(faces) == 0) : 
@@ -203,21 +136,17 @@ def dataPreprocessing(imgPath, fromMotion, toMotion):
                 with open(f"{imgPath}/{motion}.txt", "a", encoding = 'UTF8') as f:
                     f.write("\n")
                     for pointY in shapes[:,1]:
-                        f.write(str(pointY) + " ")
+                        f.write(str(pointY/height) + " ")
                 
         for _ in range(30-frameCnt):
             with open(f"{imgPath}/{motion}.txt", "a", encoding = 'UTF8') as f:
                     f.write("\n")
                     for pointY in shapes[:,1]:
-                        f.write(str(pointY) + " ")
+                        f.write(str(pointY/height) + " ")
         print(f"motion[{motion}] : detected total {frameCnt} motions \ntotal {motionCnt} motions completed ")
         
                         
 if __name__=="__main__":
-
-    model = ""
-    savePath = ""
-
     if len(sys.argv) == 1:
         print("명령 프롬프트로 실행하세요")
         exit(0)
@@ -226,9 +155,7 @@ if __name__=="__main__":
 
     elif sys.argv[1] == "train": # [train] [trainDataPath] [saveModelPath]
         train(sys.argv[2], sys.argv[3])
-    elif sys.argv[1] == "test": # [test] [modelPath] 
-        model = f"{sys.argv[2]}"
-        test(model)
+
     elif sys.argv[1] == 'data': # [data] [imgsPath] [fromMotion] [toMotion]
         dataPreprocessing(sys.argv[2], sys.argv[3], sys.argv[4])
 
