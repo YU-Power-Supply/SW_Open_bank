b0f4b27496dc2ecca7fbac7e07c1c597d4cdbb56
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2021-11-27 18:27:18 +0900

    Update O-Yes_JT.py

--- O-Yes(OpenYourEyes)/O-Yes_JT.py
+++ O-Yes(OpenYourEyes)/O-Yes_JT.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 import time
 
+
 def classificationSleepOrNonsleep(Y_Data_30_Frames):
     ## tensor generate
     Y_Data_30_Frames = tf.expand_dims(Y_Data_30_Frames, axis = 0)
@@ -11,8 +12,12 @@ def classificationSleepOrNonsleep(Y_Data_30_Frames):
 
     h = model.predict(Y_Data_30_Frames)
    
+
     print(f"Sleep [{(h[0][1]*100)//1}%]") if h.argmax() == 1 else print(f"NO Sleep [{(h[0][0])*100//1}%]")
     
+
+
+
 if __name__=="__main__":
     model = "models/output.h5"
     model = load_model(model)
@@ -20,10 +25,10 @@ if __name__=="__main__":
     
     startTime = time.time()
     frameCnt = 0
-    for Y_Data_30_Frames in lg.run(visualize=1, max_threads=4, capture=0):
+    for Y_Data_30_Frames in lg.run(visualize=1, max_threads=4, capture=0, model=-3):
         frameCnt += 1
         classificationSleepOrNonsleep(Y_Data_30_Frames)
-        
+
         # FPS Calcurator
         '''
         presendTime = time.time() - startTime 
@@ -32,3 +37,4 @@ if __name__=="__main__":
             print(f"Frames : {frameCnt*30}")
             print(f"FPS : {frameCnt*30/presendTime:.3}")
         '''
+    
